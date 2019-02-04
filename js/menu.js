197d3c7036114df46c172cf5d366593377cd6b82
McTrip777 <mcjakedude@gmail.com>
2019-02-04 14:55:28 -0800

    New repo

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+const toggleMenu = () => {
+    menu.classList.toggle("menu--open");
+};
+    const menu = document.querySelector('.menu');
+    
+    const menuButton = document.querySelector('.menu-button');
+    menuButton.addEventListener('click', toggleMenu);
+    
+    const x_it = document.querySelector('.exit');
+    x_it.addEventListener('click', toggleMenu);
+    
+    const hover = document.querySelectorAll('.menu ul li');
+        for ( i=0; i<hover.length;i++){
+        hover[i].addEventListener('mouseover', e =>{
+            e.target.style.color = "white";
+            e.target.style.backgroundColor = "black";
+        setTimeout(function() {
+        e.target.style.color = "";
+        e.target.style.backgroundColor = "";
+            }, 500);
+        }, false);
+    };
