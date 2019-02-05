0f2a8638229d088586b6d73d53f1b86be1630363
McTrip777 <mcjakedude@gmail.com>
2019-02-04 21:24:54 -0800

    design

--- js/menu.js
+++ js/menu.js
@@ -9,14 +9,14 @@ const toggleMenu = () => {
     const x_it = document.querySelector('.exit');
     x_it.addEventListener('click', toggleMenu);
     
-    const hover = document.querySelectorAll('.menu ul li');
-        for ( i=0; i<hover.length;i++){
-        hover[i].addEventListener('mouseover', e =>{
-            e.target.style.color = "white";
-            e.target.style.backgroundColor = "black";
-        setTimeout(function() {
-        e.target.style.color = "";
-        e.target.style.backgroundColor = "";
-            }, 500);
-        }, false);
-    };
+    // const hover = document.querySelectorAll('.menu ul li');
+    //     for ( i=0; i<hover.length;i++){
+    //     hover[i].addEventListener('mouseover', e =>{
+    //         e.target.style.color = "white";
+    //         e.target.style.backgroundColor = "black";
+    //     setTimeout(function() {
+    //     e.target.style.color = "";
+    //     e.target.style.backgroundColor = "";
+    //         }, 500);
+    //     }, false);
+    // };
